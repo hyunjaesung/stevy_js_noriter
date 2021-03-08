e977859b3112e21b317eee20bd2c9eeb25e8a422
hyunjae <hyunjaesung@tmon.co.kr>
2021-03-08 11:05:33 +0900

    npm start 환경설정

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -4,16 +4,21 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const PrettierPlugin = require("prettier-webpack-plugin");
 const ESLintPlugin = require("eslint-webpack-plugin");
 const paths = require("./paths");
+var glob = require('glob');
 
 module.exports = {
   // Where webpack looks to start building the bundle
-  entry: [paths.src + "/index.js"],
+  entry: glob.sync(paths.src + "/" + process.env.ROOT + "/index.**"),
 
   // Where webpack outputs the assets and bundles
   output: {
     path: paths.build,
     filename: "[name].bundle.js",
     publicPath: "/",
+  },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
 
   // Customize the webpack build process
@@ -57,14 +62,14 @@ module.exports = {
   // Determine how modules within the project are treated
   module: {
     rules: [
-      // JavaScript: Use Babel to transpile JavaScript files
-      { test: /\.js$/, use: ["babel-loader"] },
+      { test: /\.(?:js|tsx?)$/, use: ["babel-loader"] },
 
       // Images: Copy image files to build folder
       { test: /\.(?:ico|gif|png|jpg|jpeg)$/i, type: "asset/resource" },
 
       // Fonts and SVGs: Inline files
       { test: /\.(woff(2)?|eot|ttf|otf|svg|)$/, type: "asset/inline" },
+
     ],
   },
 };
---
+++ repo-starts.js
@@ -0,0 +1,44 @@
+const execSync = require('child_process').execSync;
+const inquirer = require('inquirer');
+
+const choiceSep = new inquirer.Separator();
+
+const jobs = {
+  type: 'list',
+  name: 'job',
+  message: '실행할 작업을 선택하세요.',
+  default: 'webpack-p',
+  choices: [
+    {
+        name: '🎃 Giphy로 SPA 만들기 ',
+        value: 'custom-spa-giphy-start',
+        short: '\nGiphy로 SPA 만들기',
+    },
+    {
+        name: '💀 코드스피츠 OOP MVVM 연습 ',
+        value: 'oop-js-mvvm',
+        short: '\n코드스피츠 OOP MVVM 연습',
+    },
+    {
+        name: '🦋 커스텀 리액트 연습',
+        value: 'custom-react',
+        short: '\n커스텀 리액트 연습',
+    },
+    choiceSep,
+    { name: '😢  종료', value: 'exit', short: '\n' },
+    choiceSep,
+  ],
+};
+const stdioOption = { stdio: [0, 1, 2], maxBuffer: 1024 * 500 };
+
+process.on('exit', () => console.log('\n안녕히 가세요. 다시 만나요. 👋'));
+
+
+inquirer.prompt([jobs]).then(({ job }) => {
+
+  if (job === 'exit') {
+    process.exit();
+  } else if (job) {
+    execSync(`npm run ${job}`, stdioOption);
+  }
+});
